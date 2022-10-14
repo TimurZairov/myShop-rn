a04fe412a0074d4c6bda4acdd1445de4284bd15c
Timur <zairovne@gmail.com>
2022-10-14 14:43:54 +0500

    Add Google sign in (gradle -> app & android, google-services.json) Добавил авторизацию по Google

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {
     SafeAreaView,
 } from 'react-native';
+import { GoogleSignin } from '@react-native-google-signin/google-signin'; //авторизация с помощью гугла смотреть git
+import Config from "react-native-config"; // для исаользования .env библиотека
 import Splash from "./src/screens/auth/Splash";
 import SingUp from "./src/screens/auth/Singup";
 
 
 const App: () => Node = () => {
-
+    useEffect(() => {
+        GoogleSignin.configure({
+            webClientId: Config.GOOGLE_WEB_CLIENT_ID,
+        });
+    }, [])
     return (
         <SafeAreaView style={{backgroundColor: 'white'}}>
             {/*<Splash />*/}
--- src/components/Google/index.js
+++ src/components/Google/index.js
@@ -1,13 +1,40 @@
 import React from "react";
-import {Image, View} from "react-native";
+import {Image, View, TouchableOpacity} from "react-native";
 import {styles} from './style'
+import auth from '@react-native-firebase/auth';
+import { GoogleSignin, statusCodes } from '@react-native-google-signin/google-signin';
+
+
+async function onGoogleButtonPress() {
+    // Get the users ID token
+    try {
+        const { idToken } = await GoogleSignin.signIn();
+
+        // Create a Google credential with the token
+        const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+        // Sign-in the user with the credential
+        return auth().signInWithCredential(googleCredential);
+    }catch (e) {
+        if(e.code === statusCodes.SIGN_IN_CANCELLED){
+            //user cancelled the login flow
+        } else if(e.code === statusCodes.IN_PROGRESS){
+            //operation in progress
+        }else if (e.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE){
+            //play services nor available or out date
+        }else {
+            console.log(e)
+        }
+    }
+}
+
 
 const Google = () => {
     return (
         <View style={{alignItems: 'center'}}>
-            <View style={styles.container}>
+            <TouchableOpacity activeOpacity={0.7} style={styles.container} onPress={onGoogleButtonPress}>
                 <Image resizeMode='contain' style={styles.image} source={require('../../assets/Google.png')}/>
-            </View>
+            </TouchableOpacity>
         </View>
 
     )
